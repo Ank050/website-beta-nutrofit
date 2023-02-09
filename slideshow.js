99849a6343a8b57e188739c1671bb26a7da71238
Ananya Sharma <ananyasharma.ai21@bmsce.ac.in>
2023-02-09 18:42:52 +0000

    9.2

--- slideshow.js
+++ slideshow.js
@@ -1,4 +1,54 @@
-var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+let sliderImages = document.querySelectorAll(".slide"),
+  arrowLeft = document.querySelector("#arrow-left"),
+  arrowRight = document.querySelector("#arrow-right"),
+  current = 0;
+
+// Clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+
+// Initial slide
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+// Show previous
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+// Show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", function () {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", function () {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
+
+/* var slideshows = document.querySelectorAll('[data-component="slideshow"]');
 
 // Apply to all slideshows that you define with the markup wrote
 slideshows.forEach(initSlideShow);
@@ -23,4 +73,4 @@ function initSlideShow(slideshow) {
 
     slides[index].classList.add("active");
   }, time);
-}
+} */
