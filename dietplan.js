922d4dbb5773b1663ff1f12f8ef93c4edab36791
Ankith L <ankithl135@gmail.com>
2023-03-23 20:00:29 +0000

    12.9

--- dietplan.js
+++ dietplan.js
@@ -3,33 +3,42 @@ let activeIndex = 0;
 const groups = document.getElementsByClassName("card-group");
 
 const handleLoveClick = () => {
-    const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
 
-    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
-        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
 
-    currentGroup.dataset.status = "after";
+  currentGroup.dataset.status = "after";
 
-    nextGroup.dataset.status = "becoming-active-from-before";
+  nextGroup.dataset.status = "becoming-active-from-before";
 
-    setTimeout(() => {
-        nextGroup.dataset.status = "active";
-        activeIndex = nextIndex;
-    });
-}
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+};
 
 const handleHateClick = () => {
-    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
 
-    const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
-        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
 
-    currentGroup.dataset.status = "before";
+  currentGroup.dataset.status = "before";
 
-    nextGroup.dataset.status = "becoming-active-from-after";
+  nextGroup.dataset.status = "becoming-active-from-after";
 
-    setTimeout(() => {
-        nextGroup.dataset.status = "active";
-        activeIndex = nextIndex;
-    });
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+};
+
+function myfunction() {
+  var x = document.getElementById("mydiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
